caf7563da8a391277d9c4a633475303d6d60c972
zevwings <zev.wings@gmail.com>
2021-12-15 22:42:30 +0800

    remove Pods

--- Pods/Target Support Files/ACINetworking.swift/ACINetworking.swift-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ACINetworking_swift : NSObject
-@end
-@implementation PodsDummy_ACINetworking_swift
-@end
--- Pods/Target Support Files/AnyCodable/AnyCodable-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_AnyCodable : NSObject
-@end
-@implementation PodsDummy_AnyCodable
-@end
